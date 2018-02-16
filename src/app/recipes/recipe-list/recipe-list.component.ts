819e0f9c21f8d0721f740fc038bdbffc0a5f67ec
Sandrine Delecourt <sandredlcrt@hotmail.fr>
2018-02-16 17:55:26 +0100

    display component depending on click event

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,12 +8,19 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'this is simply a test', 'https://www.eatforhealth.gov.au/sites/default/files/images/Recipes/original_lasagna_recipe_image.jpg')
+    new Recipe('A test Recipe', 'this is simply a test', 'https://www.eatforhealth.gov.au/sites/default/files/images/Recipes/original_lasagna_recipe_image.jpg'),
+    new Recipe('A test2 Recipe', 'this is simply a test2', 'https://www.eatforhealth.gov.au/sites/default/files/images/Recipes/original_lasagna_recipe_image.jpg')
   ];
+
+  @Output() recipeSelected = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  onRecipeSelected(recipe: Recipe){
+    this.recipeSelected.emit(recipe);
   }
 
 }
